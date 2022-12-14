7d6a8b136b71c60b70f8542f267fa52ed38700a4
Clark Hensley <clarkhensley@protonmail.com>
2022-12-13 22:23:07 -0600

    Forgot the audio/idle scripts

---
+++ .bin/start-audio-laptop.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Exec pipewire functions in order
+
+pipewire &
+
+pgrep -x pipewire > /dev/null
+
+while [ $? == 1 ]
+do
+    pgrep -x > pipewire /dev/null
+done
+
+pipewire-pulse &
+
+pgrep -x pipewire-pulse > /dev/null
+
+while [ $? == 1 ]
+do
+    pgrep -x > pipewire-pulse /dev/null
+done
+
+wireplumber &
+
+pgrep -x wireplumber > /dev/null
+
+while [ $? == 1 ]
+do
+    pgrep -x > wireplumber > /dev/null
+done
+
+exec custom-idle-1-laptop.sh
