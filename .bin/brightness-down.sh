d238ee2ed3f4df5982132eb69bc5e5321815a825
Clark Hensley <clarkhensley@duck.com>
2023-07-04 22:26:25 -0500

    Improving Laptop

--- .bin/brightness-down.sh
+++ .bin/brightness-down.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-brightnessctl set 5%- -q
+min_brightness=5
+
+brightness=$(brightnessctl get)
+if (( $brightness <= $min_brightness )); then
+    brightnessctl set 5% -q
+else
+    brightnessctl set 5%- -q
+fi
+
 LEVEL=$(brightnessctl get)
 echo $LEVEL > $HOME/.brightnessrc
 
